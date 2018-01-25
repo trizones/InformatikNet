94894fb8d181a7a81445a33b129c7c7782b15c97
johanfransson <johanfransson13@gmail.com>
2018-01-25 11:11:19 +0100

    BOCK

---
+++ InformatikNet/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InformatikNet.Models
+{
+    public class Test
+    {
+        public int ROMTEST { get; set; }
+    }
+}
