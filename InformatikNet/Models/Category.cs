450956b1400923e6a5dc57bfc625053a9ede7b4c
johanfransson <johanfransson13@gmail.com>
2018-01-25 13:46:30 +0100

    Post,Tag,Category added

---
+++ InformatikNet/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InformatikNet.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        //public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ InformatikNet/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InformatikNet.Models;
+
+namespace InformatikNet.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+        public virtual Tag Tags { get; set; }
+    }
+}
