9ee51c74b4e4a44f9fb2a006e90099741d238d64
Martin Sarling <martinsarling97@gmail.se>
2018-02-01 12:32:07 +0100

    Ändringar till anslag

--- InformatikNet/Models/Initializer.cs
+++ InformatikNet/Models/Initializer.cs
@@ -21,16 +21,20 @@ namespace InformatikNet.Models
             if (!roleManager.RoleExists("Administratör"))
             {
                 // first we create Admin role 
-                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "Administratör";
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole
+                {
+                    Name = "Administratör"
+                };
                 roleManager.Create(role);
 
                 //Here we create a Admin super user who will maintain the website                  
 
-                var user = new ApplicationUser();
-                user.Name = "Admin Adminsson";
-                user.UserName = "admin@hotmail.com";
-                user.Email = "admin@hotmail.com";
+                var user = new ApplicationUser
+                {
+                    Name = "Admin Adminsson",
+                    UserName = "admin@hotmail.com",
+                    Email = "admin@hotmail.com"
+                };
 
                 string userPWD = "admin123!";
 
@@ -46,8 +50,10 @@ namespace InformatikNet.Models
             // creating Creating Employee role    
             if (!roleManager.RoleExists("Anställd"))
             {
-                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "Anställd";
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole
+                {
+                    Name = "Anställd"
+                };
                 roleManager.Create(role);
 
                 //Here we create a User                 
@@ -78,35 +84,47 @@ namespace InformatikNet.Models
             var övrigt = new Category { Id = 1, CategoryName = "Övrigt", Tags = null };
             context.Category.Add(övrigt);
 
+            var anslagForskning = new Category { Id = 1, CategoryName = "AnslagForskning", Tags = null };
+            context.Category.Add(anslagForskning);
+
+            var anslagUtbildning = new Category { Id = 1, CategoryName = "AnslagUtbildning", Tags = null };
+            context.Category.Add(anslagUtbildning);
+
             for (int i = 0; i < 10; i++)
             {
-                var tag = new Tag();
-                tag.Id = i + 1;
-                tag.Name = "forskning" + i;
-                tag.Category = forskning;
-                tag.CategoryString = "forskning";
+                var tag = new Tag
+                {
+                    Id = i + 1,
+                    Name = "forskning" + i,
+                    Category = forskning,
+                    CategoryString = "forskning"
+                };
                 context.Tag.Add(tag);
                 
             }
 
             for (int i = 0; i < 10; i++)
             {
-                var tag = new Tag();
-                tag.Id = i + 1;
-                tag.Name = "utbildning" + i;
-                tag.Category = utbildning;
-                tag.CategoryString = "utbildning";
+                var tag = new Tag
+                {
+                    Id = i + 1,
+                    Name = "utbildning" + i,
+                    Category = utbildning,
+                    CategoryString = "utbildning"
+                };
                 context.Tag.Add(tag);
 
             }
 
             for (int i = 0; i < 10; i++)
             {
-                var tag = new Tag();
-                tag.Id = i + 1;
-                tag.Name = "övrigt" + i;
-                tag.Category = övrigt;
-                tag.CategoryString = "övrigt";
+                var tag = new Tag
+                {
+                    Id = i + 1,
+                    Name = "övrigt" + i,
+                    Category = övrigt,
+                    CategoryString = "övrigt"
+                };
                 context.Tag.Add(tag);
 
             }
